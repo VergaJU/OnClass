64eaf6067aca74e24038359180d4456e2859f669
VergaJU <jacopoumberto.verga@gmail.com>
2022-07-12 16:07:48 +0100

    Created repo

---
+++ config.py
@@ -0,0 +1,15 @@
+repo_dir = '/home/jacopoverga/Desktop/OnClass/OnClass'
+data_dir = '/home/jacopoverga/Desktop/OnClass/OnClass_data_public'
+
+scrna_data_dir = data_dir + '/scRNA_data/'
+ontology_data_dir = data_dir + '/Ontology_data/'
+intermediate_dir = data_dir + '/Intermediate_files/'
+
+result_dir = repo_dir + '/result/'
+figure_dir = repo_dir + '/result/Figure/'
+model_dir = data_dir + '/Pretrained_model/'
+
+Run_scanorama_batch_correction = False
+NHIDDEN = [100]
+MAX_ITER = 10
+MEMORY_SAVING_MODE = True
