9aee40a2468ec60b51684b835ac359575c38ff3b
VergaJU <jacopoumberto.verga@gmail.com>
2022-07-13 19:20:32 +0100

    model update

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 repo_dir = '/home/jacopo/Documents/GitHub/OnClass/test'
-data_dir = '/home/jacopo/Documents/GitHub/OnClass/OnClass_data_public'
+data_dir = '/home/jacopo/Documents/GitHub/OnClass/'
 
-scrna_data_dir = data_dir + '/scRNA_data/'
-ontology_data_dir = data_dir + '/Ontology_data/'
-intermediate_dir = data_dir + '/Intermediate_files/'
+scrna_data_dir = data_dir + 'test/'
+ontology_data_dir = data_dir + 'OnClass_data_public/Ontology_data/'
+intermediate_dir = data_dir + 'OnClass_data_public/Intermediate_files/'
 
 result_dir = repo_dir + '/result/'
 figure_dir = repo_dir + '/result/Figure/'
@@ -11,5 +11,5 @@ model_dir = data_dir + '/Pretrained_model/'
 
 Run_scanorama_batch_correction = False
 NHIDDEN = [100]
-MAX_ITER = 100
+MAX_ITER = 500
 MEMORY_SAVING_MODE = True
